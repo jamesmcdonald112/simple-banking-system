782b7bd073baa206cb9669afa176ce56576f6ce2
James McDonald <jamesmcdonald112@yahoo.ie>
2025-06-30 14:01:38 +0100

    feat: add Account class with default balance and test

--- Simple Banking System (Java)/task/src/banking/account/Account.java
+++ Simple Banking System (Java)/task/src/banking/account/Account.java
@@ -1,4 +1,18 @@
 package banking.account;
 
 public class Account {
+    private int balance;
+
+    public Account() {
+        this.balance = 0;
+    }
+
+    /**
+     *Returns the current balance of the account
+     *
+     * @return the current account balance as an int
+     */
+    public int getBalance() {
+        return balance;
+    }
 }

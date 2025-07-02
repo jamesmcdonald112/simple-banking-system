274e37c549fb7278466c4b22ec2dc69dee627af8
James McDonald <jamesmcdonald112@yahoo.ie>
2025-07-02 17:04:37 +0100

    test: verify AccountStore returns null for incorrect card or pin

--- Simple Banking System (Java)/task/test/banking/account/AccountStoreTest.java
+++ Simple Banking System (Java)/task/test/banking/account/AccountStoreTest.java
@@ -68,8 +68,25 @@ public class AccountStoreTest {
 
         assertNotNull("The retrieved account should not be null",
                 retrievedAccount);
+
         assertEquals("The retrieved account should match the originally added account",
                 account,
                 retrievedAccount);
+    }
+
+    @Test
+    public void testFindByIncorrectCardOrPinReturnsNull() {
+        Account account = new Account();
+        AccountStore.addAccount(account);
+
+        Account dummyAccount = new Account(); // not added
+
+        //Wrong card number
+        assertNull("The retrieved account should be null as the card does not match",
+                AccountStore.findByCardAndPin(dummyAccount.getCardNumber(), account.getPin()));
+
+        // Wrong PIN
+        assertNull("The retrieved account should be null as the pin does not match",
+                AccountStore.findByCardAndPin(account.getCardNumber(), dummyAccount.getPin()));
     }
 }

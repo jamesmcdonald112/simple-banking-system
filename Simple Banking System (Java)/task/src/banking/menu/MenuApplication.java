c42e0e3656aa0569f0e3af9c994e8630dcde7bf9
James McDonald <jamesmcdonald112@yahoo.ie>
2025-07-04 20:35:16 +0100

    Refactor: extract account info printing + add Javadoc to MenuApplication

--- Simple Banking System (Java)/task/src/banking/menu/MenuApplication.java
+++ Simple Banking System (Java)/task/src/banking/menu/MenuApplication.java
@@ -1,7 +1,6 @@
 package banking.menu;
 
 import banking.account.Account;
-import banking.account.AccountStore;
 
 import java.util.Scanner;
 
@@ -13,6 +12,7 @@ public class MenuApplication {
     }
 
 
+    /** Starts the main menu loop and handles user navigation. */
     public void start() {
         boolean running = true;
 
@@ -31,6 +31,7 @@ public class MenuApplication {
 
     }
 
+    /** Prints the main menu options to the console. */
     private void printMenuOptions() {
         System.out.println("""
                 1. Create an account
@@ -38,11 +39,23 @@ public class MenuApplication {
                 0. Exit""");
     }
 
+    /**
+     * Creates a new account, stores it in AccountStore,
+     * and prints the account details to the console.
+     */
     private void handleCreateAccount() {
         Account createdAccount = MenuController.handleCreateAccountOption();
-        String cardNumber = createdAccount.getCardNumber();
-        String pin = createdAccount.getPin();
+        printAccountInformation(createdAccount.getCardNumber(), createdAccount.getPin());
 
+    }
+
+    /**
+     * Prints the generated card number and PIN to the console.
+     *
+     * @param cardNumber The generated card number
+     * @param pin        The generated PIN
+     */
+    private void printAccountInformation(String cardNumber, String pin) {
         System.out.println("Your card has been created");
         System.out.println("Your card number:\n" + cardNumber);
         System.out.println("Your card PIN:\n" + pin);

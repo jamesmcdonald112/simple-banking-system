bdf5090fc9d5695fe615c8785a42d4b052d3e58a
James McDonald <jamesmcdonald112@yahoo.ie>
2025-07-04 09:29:03 +0100

    test: add test for invalid input for MenuService

--- Simple Banking System (Java)/task/test/banking/menu/MenuServiceTest.java
+++ Simple Banking System (Java)/task/test/banking/menu/MenuServiceTest.java
@@ -10,9 +10,9 @@ public class MenuServiceTest {
      * Verifies that an input of "0" should return EXIT
      */
     @Test
-    public void testUserInput1ReturnsExit() {
+    public void testUserInput0ReturnsExit() {
         MenuResult menuResult = MenuService.handleMenuInput("0");
-        assertEquals("An input of 0 should return CREATE_ACCOUNT",
+        assertEquals("An input of 0 should return EXIT",
                 MenuResult.EXIT,
                 menuResult);
     }
@@ -32,11 +32,29 @@ public class MenuServiceTest {
      * Verifies that an input of "2" should return LOGIN
      */
     @Test
-    public void testUserInput1ReturnsLogin() {
+    public void testUserInput2ReturnsLogin() {
         MenuResult menuResult = MenuService.handleMenuInput("2");
         assertEquals("An input of 2 should return LOGIN",
                 MenuResult.LOGIN,
                 menuResult);
+    }
+
+    /**
+     * Verifies that INVALID is returned for invalid inputs
+     */
+    @Test
+    public void testInvalidInputReturnsInvalid() {
+        assertEquals("Input of '999' should be invalid",
+                MenuResult.INVALID,
+                MenuService.handleMenuInput("999"));
+
+        assertEquals("Input of abc should be invalid",
+                MenuResult.INVALID,
+                MenuService.handleMenuInput("abc"));
+
+        assertEquals("Input of '' should be invalid",
+                MenuResult.INVALID,
+                MenuService.handleMenuInput(""));
     }
 
 }

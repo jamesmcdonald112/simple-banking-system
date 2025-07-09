3c6756abc94da91bfd0311c84543dd7a575ffa5e
James McDonald <jamesmcdonald112@yahoo.ie>
2025-07-09 19:41:59 +0100

    feat: runner creates database on startup with database name extracted from command-line arguements

--- Simple Banking System (Java)/task/src/banking/runner/Runner.java
+++ Simple Banking System (Java)/task/src/banking/runner/Runner.java
@@ -1,5 +1,7 @@
 package banking.runner;
 
+import banking.database.CardDAO;
+import banking.database.DatabaseConfig;
 import banking.menu.main.MainMenuApplication;
 
 import java.util.Scanner;
@@ -7,6 +9,10 @@ import java.util.Scanner;
 public class Runner {
 
     public static void main(String[] args) {
+        String dbName = DatabaseConfig.parseDbFileName(args);
+        CardDAO dao = new CardDAO(dbName);
+        dao.ensureCardTableExists();
+
         Scanner scanner = new Scanner(System.in);
         MainMenuApplication menu = new MainMenuApplication(scanner);
         menu.start();
